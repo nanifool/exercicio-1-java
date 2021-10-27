6bb84dc21d2cb56ef9c574794bc91e571fb6250f
Luana da Rosa <lrosa.silva@outlook.com>
2021-10-27 16:10:15 -0300

    adionado o enunciado e #TODO

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -28,3 +28,33 @@ public class Main {
         return qtd;
     }
 }
+
+//2 ) Construa os seguintes métodos sobre arranjos
+//        a. Método que retorna quantas ocorrências de um elemento estão na lista
+public static int nOcorrencias(int[] l, Integer el) {
+
+}
+//        b. Método que retorna true se l tem elementos repetidos
+public static boolean hasRepeat(int[] l){
+
+}
+//        c. Método que retorna o número de elementos repetidos em l
+public static int nroRepeat(int[] l){
+
+}
+//        d. Método que retorna uma lista de elementos repetidos de l
+public static int[] listRepeat(int[] l){
+
+}
+//        e. Método que retorna a união de l1 e l2
+public static int[] union(int[] l1, int[] l2){
+
+}
+//        f. Método que retorna a intersecção de l1 e l2
+public static int[] intersect(int[] l1, int[] l2){
+
+}
+//        g. Método que retorna a diferença entre l1 e l2
+public static int[] difference(int[] l1, int[] l2){
+
+}
