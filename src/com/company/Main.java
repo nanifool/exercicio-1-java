e9895aaa1d37889ce98546b6bdebd83274bd817c
Luana da Rosa <lrosa.silva@outlook.com>
2021-10-28 13:31:09 -0300

    resolução de alguns exercicios

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,20 +2,28 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         int[] list = new int[20];
-        int [] newList = new int[20];
         int i;
 
-        for(i=0; i < list.length; i++) {
+        System.out.println("\nlista:");
+        for(i=0; i < list.length; i++){
             list[i] = i * 10;
             System.out.println(list[i]);
         }
 
-        //TODO: inverter array
+        System.out.println("\nlista invertidade:");
 
-        System.out.println("/////////");
+        for(i= list.length -1; i>=0; i--){
+            System.out.println(list[i]);
+        }
+
+        System.out.println("\nnumero de ocorrencias");
         System.out.println(nOcorrencias(list, 20));
+        System.out.println("\nelementos repetidos");
+        System.out.println(hasRepeat(list));
+        System.out.println("\nnumeros repetidos");
+        System.out.println(nroRepeat(list));
     }
 
     public static int nOcorrencias(int[] l, int el){
@@ -27,34 +35,49 @@ public class Main {
         }
         return qtd;
     }
-}
 
-    //2 ) Construa os seguintes métodos sobre arranjos
-    //        a. Método que retorna quantas ocorrências de um elemento estão na lista
-    public static int nOcorrencias(int[] l, Integer el) {
-    //TODO: escrever aqui
-    }
     //        b. Método que retorna true se l tem elementos repetidos
     public static boolean hasRepeat(int[] l){
-    //TODO: escrever aqui
+        boolean nRepetead = false;
+        for(int i=0; i < l.length; i++ ){
+            for(int j=0; j < i; j++){
+                if (l[i] == l[j]){
+                    nRepetead = true;
+                }
+            }
+        }
+        return nRepetead;
     }
     //        c. Método que retorna o número de elementos repetidos em l
     public static int nroRepeat(int[] l){
-    //TODO: escrever aqui
+        int nRepetead = 0;
+        for(int i=0; i < l.length; i++){
+            for (int j=0; j < l.length; j++){
+                if (l[i] == l[j]){
+                    nRepetead++;
+                }
+            }
+        }
+        return nRepetead;
     }
-    //        d. Método que retorna uma lista de elementos repetidos de l
-    public static int[] listRepeat(int[] l){
-    //TODO: escrever aqui
-    }
-    //        e. Método que retorna a união de l1 e l2
-    public static int[] union(int[] l1, int[] l2){
-    //TODO: escrever aqui
-    }
-    //        f. Método que retorna a intersecção de l1 e l2
-    public static int[] intersect(int[] l1, int[] l2){
-    //TODO: escrever aqui
-    }
-    //        g. Método que retorna a diferença entre l1 e l2
-    public static int[] difference(int[] l1, int[] l2){
-    //TODO: escrever aqui
-    }
+
+    //TODO: verificar se o exercicio acima, b e c, estão corretos.
+
+
+//    //        d. Método que retorna uma lista de elementos repetidos de l
+//    public static int[] listRepeat(int[] l){
+//    //TODO: escrever aqui
+//    }
+//    //        e. Método que retorna a união de l1 e l2
+//    public static int[] union(int[] l1, int[] l2){
+//    //TODO: escrever aqui
+//    }
+//    //        f. Método que retorna a intersecção de l1 e l2
+//    public static int[] intersect(int[] l1, int[] l2){
+//    //TODO: escrever aqui
+//    }
+//    //        g. Método que retorna a diferença entre l1 e l2
+//    public static int[] difference(int[] l1, int[] l2){
+//    //TODO: escrever aqui
+//    }
+}
